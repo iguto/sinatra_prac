db51db632cb8e02e862b2e9f328bda5f56d06505
iguto <clc.iguto@gmail.com>
2015-01-30 20:17:09 +0900

    catch up

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 require 'bundler'
 Bundler.require
 
+# トップページ
 get '/' do
   "Hello Sinatra"
+end
+
+# トップ以外のページ
+get '/hoge' do
+  "hoge"
+end
+
+# パラメータの受け取り
+get '/app/:hoge' do
+  "got param: #{params[:hoge]}"
+end
+
+# htmlの表示
+get '/form' do
+"<h2>This is a header!!!!</h2>
+this isnt header"
 end
